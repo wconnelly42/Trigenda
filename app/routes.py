71822109938b275c323cd6bdbbcd8217c9f97d6e
EC2 Default User <ec2-user@ip-172-31-46-207.us-east-2.compute.internal>
2019-07-31 18:50:38 +0000

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,8 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template("index.html")
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
